71020454ebd6b655dc63ad9f4eec1fa54d649565
Ali Asgar Merchant <am52saif@gmail.com>
2018-02-19 00:50:24 -0500

    Added extra angular components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
+import { User } from './shared/models/user';
 
 @Component({
   selector: 'my-app',
-  styles: [`
-    
-  `],
-  template: `
-    <div class="jumbotron text-center">
-      <h1>The App Lives!</h1>
-      <p>{{ message }}</p>
-    </div>
-  `
+  templateUrl: './app/app.component.html',
+  styleUrls: ['./app/app.component.css']
 })
 export class AppComponent {
-  message = 'This is the sample message.';
+  message: string = 'Hello!'; 
+  users: User[] = [
+    { id: 25, name: 'Chris', username: 'sevilayha' },
+    { id: 26, name: 'Nick', username: 'whatnicktweets' },
+    { id: 27, name: 'Holly', username: 'hollylawly' }
+  ];
+  activeUser: User;
+
+  selectUser(user) {
+    this.activeUser = user;
+    console.log(this.activeUser);
+  }
+
+  onUserCreated(event) {
+    this.users.push(event.user);
+  }
 }
